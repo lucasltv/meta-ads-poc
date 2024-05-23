81f942c7e79331726aa579eef65706e3bbaac8a6
Lucas Vieira <lucasltv@gmail.com>
2024-05-23 16:50:23 -0300

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 import dotenv from 'dotenv';
-import {
-  AdAccount,
-  Campaign,
-  FacebookAdsApi,
-} from 'facebook-nodejs-business-sdk';
+import {AdAccount, FacebookAdsApi} from 'facebook-nodejs-business-sdk';
 dotenv.config();
 
 const {META_ACCESS_TOKEN, META_ACCOUNT_ID} = process.env;
@@ -13,10 +9,10 @@ FacebookAdsApi.init(META_ACCESS_TOKEN!);
 const start = async () => {
   try {
     const account = new AdAccount(META_ACCOUNT_ID);
-    const campaigns = await account.getCampaigns([
-      Campaign.Fields.name,
-      Campaign.Fields.status,
-    ]);
+    const campaigns = await account.getCampaigns([], {
+      limit: 10,
+      fields: ['id', 'name'],
+    });
     debugger;
   } catch (error) {
     debugger;
