39355e43343dd2bba658eba8db525c64ae4025c5
Lucas Vieira <lucasltv@gmail.com>
2024-05-24 16:29:15 -0300

    refreshAccessToken

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,33 @@ import {
 } from 'facebook-nodejs-business-sdk';
 dotenv.config();
 
-const {META_ACCESS_TOKEN, META_ACCOUNT_ID} = process.env;
+const {META_ACCESS_TOKEN, META_APP_ID, META_APP_SECRET, META_ACCOUNT_ID} =
+  process.env;
 
 FacebookAdsApi.init(META_ACCESS_TOKEN!);
 const account = new AdAccount(`act_${META_ACCOUNT_ID}`);
+
+const refreshAccessToken = async () => {
+  try {
+    const params = `grant_type=fb_exchange_token&client_id=${META_APP_ID}&client_secret=${META_APP_SECRET}&fb_exchange_token=${META_ACCESS_TOKEN}`;
+
+    const response = await fetch(
+      `https://graph.facebook.com/v12.0/oauth/access_token?${params}`,
+      {method: 'GET'}
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data: any = await response.json();
+    const longLivedAccessToken = data.access_token;
+    console.log('New Long-lived Access Token:', longLivedAccessToken);
+  } catch (error) {
+    console.error('Error renewing access token:', error);
+    throw error;
+  }
+};
 
 const getActiveCampaigns = async () => {
   try {
@@ -59,4 +82,5 @@ const start = async () => {
   }
 };
 
-start();
+refreshAccessToken();
+// start();
